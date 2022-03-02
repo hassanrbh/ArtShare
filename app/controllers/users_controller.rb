d6f3dc9679479b81b9ef712096e4dbae3e28e7fe
hassantarif <hassantarif4@gmail.com>
2022-03-02 13:28:32 +0100

    users model Accepted

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+    def index
+        # /users
+        users = User.all
+        render :json => users
+    end
+    def show
+        # /users/:id        
+        user = User.find_by(:id => params[:id])
+        render :json => user
+    end
+    def create
+        # /users/new
+        user = User.new(user_params)
+        if user.save
+            render :json => user
+        else
+            render :json => user.errors.full_messages, :status => :unprocessable_entity
+        end
+    end
+    def update
+        # /users/:id
+        user = User.find_by(:id => params[:id])
+        if user.update(user_params)
+            render :json => user
+        else
+            render :json => user.errors.full_messages, :status => :unprocessable_entity
+        end
+    end
+
+    protected
+
+    def user_params
+        params.require(:users).permit(:username)
+    end
+end

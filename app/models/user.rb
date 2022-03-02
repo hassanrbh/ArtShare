a8113a8820215d30813d51ebfcdd47f2a070e5bb
hassantarif <hassantarif4@gmail.com>
2022-03-02 21:02:00 +0100

    seed the db

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,13 @@ class User < ApplicationRecord
     has_many :artworks,
         class_name: "Artwork",
         primary_key: :id,
-        foreign_key: :artist_id
+        foreign_key: :artist_id,
         dependent: :destroy
         
     has_many :artwork_shares,
         class_name: "ArtworkShare",
         primary_key: :id,
-        foreign_key: :viewer_id
+        foreign_key: :viewer_id,
         dependent: :destroy
 
     has_many :shared_viewers,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Drop All The Tables
+User.destroy_all
+Artwork.destroy_all
+ArtworkShare.destroy_all
+
+# User Data
+
+hassan = User.create!(:username => "hassan")
+salman = User.create!(:username => "salman")
+aya = User.create!(:username => "aya")
+najat = User.create!(:username => "najat")
+
+# Artwork Data
+
+hassan_artwork = Artwork.create!(
+    :title => "Whoops",
+    :image_url => "https://imgur.com/gallery/ya873n0",
+    :artist_id => hassan.id
+)
+
+salman_artwork = Artwork.create!(
+    :title => "Persian Empire",
+    :image_url => "https://imgur.com/gallery/0wzRsEF",
+    :artist_id => salman.id
+)
+
+aya_artwork = Artwork.create!(
+    :title => "Perfect",
+    :image_url => "https://imgur.com/gallery/7Cv7AHa",
+    :artist_id => aya.id
+)
+
+najat_artwork = Artwork.create!(
+    :title => "The foundation of politics",
+    :image_url => "https://imgur.com/gallery/9zjDM4P",
+    :artist_id => najat.id
+)
+
+# Artwork Share
+
+artwork_share_1 = ArtworkShare.create!(
+    :artwork_id => hassan_artwork.id,
+    :viewer_id => salman.id
+)
+
+artwork_share_2 = ArtworkShare.create!(
+    :artwork_id => salman_artwork.id,
+    :viewer_id => hassan.id
+)
+
+artwork_share_3 = ArtworkShare.create!(
+    :artwork_id => aya_artwork.id,
+    :viewer_id => najat.id
+)
+
+artwork_share_1 = ArtworkShare.create!(
+    :artwork_id => najat_artwork.id,
+    :viewer_id => aya.id
+)

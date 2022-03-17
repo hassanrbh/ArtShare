739de2271092110c8b73f158fce664a6123882f9
hassantarif <hassantarif4@gmail.com>
2022-03-17 13:09:17 +0100

    fi us

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,9 @@ class User < ApplicationRecord
     validates :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true, uniqueness: true
     validates :password, length: {minimum: 6, message: 'must be at least 6 characters'}
+    after_initialize :ensure_session_token
 
+    # make the password encrypted and hash it with BCrypt
     def password=(password) # convert the current password to an encrypted version of it
         self.password_digest = BCrypt::Password.create(password)
         @password = password
@@ -25,4 +27,32 @@ class User < ApplicationRecord
     def is_password!(password)
         BCrypt::Password.new(self.password_digest).is_password?(password)
     end
+
+    # Make a session token
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.session_token        
+    end
+
+    # Check if the user in the db
+    
+    def self.find_by_credentials!(email,password)
+        user = User.find_by(:email => email)
+        return nil if user.nil?
+        if user.is_password!(password)
+            user
+        end
+    end
+
+    private 
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+    
 end

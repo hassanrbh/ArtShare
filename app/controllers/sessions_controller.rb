28b64d29cff1dcb03805130fb2cc04ce79d780a5
hassantarif <hassantarif4@gmail.com>
2022-03-20 23:58:08 +0100

    fixe some styling changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 
         if user.present?
             log_in_user!(user)
-            redirect_to user_path(user), :success => "Welcome Again #{user.email}"
+            redirect_to bands_path, :success => "Welcome Again #{user.email}"
         else
             redirect_to new_session_path, :error => "credentials failed"
         end

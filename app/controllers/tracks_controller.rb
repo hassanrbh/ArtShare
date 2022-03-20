2f0351af47a1958b31d52f922e9a035e32f10c9e
hassantarif <hassantarif4@gmail.com>
2022-03-20 17:46:40 +0100

    changes in style of the webpage and add tracks controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,34 @@
 class TracksController < ApplicationController
+    def index
+    end
+    def show
+        @track = Track.find_by(:id => params[:id])
+        render :show
+    end
+    def new
+        @album = Album.find_by(:id => params[:album_id])
+        render :new
+    end
+    def create
+        @track = Track.new(tracks_params)
+        if @track.save
+            flash.now[:success] = "Track created successfully"
+            redirect_to track_path(@track.id)
+        else
+            flash[:errors] = @track.errors.full_messages
+            redirect_to album_path(@track.album.id)
+        end
+    end
+
+    private
+
+    def tracks_params
+        params.require(:tracks).permit(
+            :title,
+            :album_id,
+            :ord,
+            :lyrics,
+            :track_type
+        )
+    end
 end
